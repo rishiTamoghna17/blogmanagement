3e6a6704f0f6eddf3fe340fd2883272dc0ca9a1d
rishiTamoghna17 <tamoghna171099@gmail.com>
2022-09-08 11:14:21 +0530

    tamogh

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -32,34 +32,23 @@ const authenticate = async function (req, res, next) {
 
 const authorization = async function (req, res, next) {
   try {
-    let token = req.headers["x-api-key"];
-        let decodedToken = jwt.verify(
-            token,
-            "suraj_tamoghna_kashish_tanweer"
-          );
-      let blogToBeModified = decodedToken.userId//1st author id
-      console.log(blogToBeModified)
-      let blogId = req.params.blogId
-      let blog = await blogModel.findOne(blogId)
-      console.log(blog)
-      let userLoggedIn = blog.authorId
-      console.log(userLoggedIn)
-       if (!mongoose.isValidObjectId(blogToBeModified)) {
-           return res.status(404).send({ status: false, msg: "invalid blogId" });
-       }
-       if(blogToBeModified != userLoggedIn) return res.send({status: false, msg: 'User logged is not allowed to modify the requested users data'})
-  next();
-}
+    const token = req.headers['x-api-key']
+    if (!token) res.status(401).send({ status: false, msg: "missing a mandatory token😒" })
+    let decodedToken = jwt.verify(
+                  token,
+                  "suraj_tamoghna_kashish_tanweer"
+                )
+                let authorId = decodedToken.authorId
+    let blog = req.params.blogId
+    let blogData = await blogModel.findOne({ _id: blog })
+    console.log(userId)
+    console.log(blogData.authorId.toString())
 
-      // let blog = await authorModel.findById({ _id: blogToBeModified })
-      // console.log(blog)
-      // if (blog) {
-      //     if (blog.authorId !== blogToBeModified) {
-      //         return res.status(403).send({ status: false, msg: 'Author logged is not allowed to modify the requested data' })
-    //     } else 
-      //         next()
-      //     }
-      
+    if (blogData.authorId.toString() != authorId) {
+      return res.status(403).send({ status: false, msg: 'You are not authrized' })
+    }
+    next()
+  }  
   catch (error) {
       res.status(500).send({ status: false, Error: error.message })
   }
