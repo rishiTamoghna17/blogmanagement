8c227b02b854f9c684c300082e6aba94d45915a2
suraj <surajkumargt7262@gmail.com>
2022-09-09 16:03:21 +0530

    surajupdate

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -46,15 +46,19 @@ const authorization = async function (req, res, next) {
 const specialAuthorization = async function (req, res, next){
   try {
     let data = req.query;
-    let filter = {...data};
-    const { authorId, category, tags, subcategory } = data
+    
     const token = req.headers["x-api-key"]; // we call headers with name x-api-key
     if (!token)
       res.status(401).send({ status: false, msg: "missing a mandatory token😒" });
       let decodedToken = jwt.verify(token, "suraj_tamoghna_kashish_tanweer");
       let userLoggedIn = decodedToken.userId;
+      let filter = {userLoggedIn,...data};
+    const { authorId, category, tags, subcategory } = data
       let getRecord = await blogModel.findOne(filter)
+      console.log(getRecord)
+      console.log(userLoggedIn )
       let userId = getRecord.authorId.toString();
+      console.log(userId)
       if(userId.toString() != userLoggedIn) {
         return res.status(403).send({ status: false, msg: "You are not authrized" });
       }
@@ -63,5 +67,6 @@ next();
   res.status(500).send({ status: false, Error: error.message });
 }
 }
+
 
 module.exports = { authenticate, authorization,specialAuthorization };
